d3d2ae37d01492aa9806feb3aa3dfb4dc558f681
norazics <nora.zics@yahoo.co.uk>
2023-04-03 18:51:24 +0100

    added database connection setup

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// Creating a base name for the MongoDB
+const mongooseBaseName = 'loveletters';
+
+// Create the MongoDB URI for Development and Test
+const database = {
+    development: `mongodb://localhost:27017/${mongooseBaseName}-development`,
+    test: `mongodb://localhost:27017/${mongooseBaseName}-test`
+  };
+
+// Indentify if development environment is Test or Development
+// Select a Database based on whether a test file was executed before `server.js`
+const localDB = process.env.TESTENV ? database.test : database.development;
+
+// Environment variable MONGODB_URI will be avaiable in
+// Heroku production environment. Otherwise use Test or
+// Development database.
+const currentDB = process.env.MONGODB_URI || localDB;
+
+// Export the approprate database based on the current environment
+module.exports = currentDB; 
