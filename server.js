bdf7a25442e10a2657fa62df56bbb671e14cd014
norazics <nora.zics@yahoo.co.uk>
2023-04-03 16:53:20 +0100

    add index route

--- server.js
+++ server.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Require Route Files
+const indexRouter = require('./routes/index');
+
 // Instantiate Express Application Object
 const app = express();
 
 // Define PORT for the API to run on -  If the PORT environment variable is not set, the server will default to port 5000
 const port = process.env.PORT || 5000;
+
+/** 
+ * Routes
+ * 
+ * Mount imported Routers
+*/
+app.use(indexRouter);
 
 // Start the server and listen for requests on the given port
 app.listen(port, () => console.log(`listening on port ${port}`));
