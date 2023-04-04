3aa02bc6a2d1e5a2796b7c48339f121c06738a3c
norazics <nora.zics@yahoo.co.uk>
2023-04-04 12:28:52 +0100

    added JSON middleware

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ const app = express();
 // Define PORT for the API to run on -  If the PORT environment variable is not set, the server will default to port 5000
 const port = process.env.PORT || 5000;
 
+/** Middleware */
+
+// Add `bodyParser` middleware which will parse JSON
+// requests into JavaScript Objects before they reach
+// the route files.
+//
+// The method `.use` sets up middleware for Express apps.
+app.use(express.json());
+
 /** 
  * Routes
  * 
