feffe1118fb5de7312f09eb7a623d97bfbd1e8db
norazics <nora.zics@yahoo.co.uk>
2023-04-05 18:49:41 +0100

    added CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require neccessary NPM Packages
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 // Require DB Configuration File
 const db = require('./config/db');
@@ -20,11 +21,17 @@ const app = express();
 
 // Define PORT for the API to run on -  If the PORT environment variable is not set, the server will default to port 5000
 const port = process.env.PORT || 5000;
+const reactPort = 3000;
 
 /** Middleware */
 // The method `.use` sets up middleware for Express apps.
 app.use(express.json());
 
+// Set CORS headers on responses frpmo this API using 'cors' NPM package
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`
+  }));
+  
 /** 
  * Routes
  * 
