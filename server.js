855536bbbf8c901d41ce515efd25189895b3c0ba
norazics <nora.zics@yahoo.co.uk>
2023-04-10 14:24:22 +0100

    mounted bcrypt jsnonwebtoken passport passport-jwt

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 
+// Require Auth Related Packages
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
 // Require DB Configuration File
 const db = require('./config/db');
 
@@ -10,11 +15,19 @@ const db = require('./config/db');
 mongoose.connect(db, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 
+// Require Passport Strategy and Options
+const strategy = require('./lib/passportStrategy');
+const jwtOptions = require('./lib/passportOptions');
+
+const saltRounds = 10;
+
 // Require Route Files
 const indexRouter = require('./routes/index');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 const messageRouter = require('./routes/messages');
+const authRouter = require('./routes/Auth');
+
 
 // Instantiate Express Application Object
 const app = express();
@@ -27,11 +40,14 @@ const reactPort = 3000;
 // The method `.use` sets up middleware for Express apps.
 app.use(express.json());
 
-// Set CORS headers on responses frpmo this API using 'cors' NPM package
+// Set CORS headers on responses from this API using 'cors' NPM package
 app.use(cors({
     origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`
   }));
-  
+
+// Define auth strategy from before - mount this strategy
+passport.use(strategy);
+
 /** 
  * Routes
  * 
@@ -41,6 +57,10 @@ app.use(indexRouter);
 app.use(cardsRouter);
 app.use(usersRouter);
 app.use(messageRouter);
+  
+// Authentication
+app.use(authRouter);
+
 
 // Start the server and listen for requests on the given port
 app.listen(port, () => console.log(`listening on port ${port}`));
